7d1be007e5e16c54718ced9888225f57fbe2d5a7
JKimbap <jayjinyoung0822@gmail.com>
2025-04-13 12:15:37 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -18,32 +18,16 @@ def hypotenuse(a,b):
 def add(a, b):
     return a + b
 
-def sub(a, b):
-    return a - b
-
 def subtract(a, b):
     return a - b
 
 def mul(a, b):
     return a*b
 
-def multiply(a, b):
-    return a * b
-
 def div(a, b):
     if a == 0:
         raise ZeroDivisionError
     return b / a
-
-def divide(a, b):
-    if a == 0:
-        raise ZeroDivisionError
-    return b / a
-
-def log(a, b):
-    if a <= 0 or a == 1 or b <= 0:
-        raise ValueError("Logarithm is undefined for these values.")
-    return math.log(b, a)
 
 def logarithm(a, b):
     if a <= 0 or a == 1 or b <= 0:
@@ -51,7 +35,4 @@ def logarithm(a, b):
     return math.log(b,a)
 
 def exp(a, b):
-    return a**b
-
-def exponent(a, b):
     return a**b
