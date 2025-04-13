5428417dcc547698925be0c80cd55acff3799541
JKimbap <jayjinyoung0822@gmail.com>
2025-04-13 11:29:18 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -6,7 +6,6 @@ One function per operation, in order.
 """
 # First example
 import math
-
 def add(a, b):
     a + b
 
@@ -17,10 +16,14 @@ def multiply(a, b):
     a * b
 
 def divide(a, b):
-    b / a   # raise ZeroDivisionError if a == 0
+    if a == 0:
+        raise ZeroDivisionError("Can not divide by a zero.")
+    return b / a   # raise ZeroDivisionError if a == 0
 
-def loga(a, b):
-    loga(b)# use math library/raise ValueError
+def logarithm(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError("Logarithm is undefined for these values.")
+    return math.log(b,a)# use math library/raise ValueError
 
 def exponent(a, b):
     a**b
