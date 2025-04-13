a9f57f75179da3bf4e1230fc82595f4680e1179d
ratinmyravioli <erikapoirier@ufl.edu>
2025-04-13 12:02:40 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -9,26 +9,40 @@ import math
 def add(a, b):
     return a + b
 
+def sub(a, b):
+    return a - b
+
 def subtract(a, b):
     return a - b
 
+def mul(a, b):
+    return a*b
+
 def multiply(a, b):
     return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b / a
 
 def divide(a, b):
     if a == 0:
         raise ZeroDivisionError
     return b / a
 
-def loga(a, b):
+def log(a, b):
     if a <= 0 or a == 1 or b <= 0:
-        raise ValueError
+        raise ValueError("Logarithm is undefined for these values.")
     return math.log(b, a)
 
 def logarithm(a, b):
     if a <= 0 or a == 1 or b <= 0:
         raise ValueError("Logarithm is undefined for these values.")
     return math.log(b,a)
+
+def exp(a, b):
+    return a**b
 
 def exponent(a, b):
     return a**b
